6ea2116a8e034627cf02df353cc4b72a148b0b02
Suvankar Das <iambabai0@gmail.com>
2021-04-11 03:28:01 +0530

    Books Controller with in memory data

---
+++ LibraryService/LibraryService/Controllers/BooksController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using LibraryService.Models;
+
+namespace LibraryService.Controllers
+{
+    public class BooksController : ApiController
+    {
+        public IEnumerable<Book> Books = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1, Title = "The Girl on the Train", Author = "Hawkins, Paula", PublicationYear = 2015,
+                CallNumber = "F HAMKI"
+            },
+
+            new Book
+            {
+                Id = 2, Title = "Rogue Lawyer", Author = "Grisham, John", PublicationYear = 2015, CallNumber = "F GRISH"
+            },
+
+            new Book
+            {
+                Id = 3, Title = "After You", Author = "Moyes, Jojo", PublicationYear = 2015, CallNumber = "F MOYES"
+            },
+
+            new Book
+            {
+                Id = 4, Title = "All the Light We Cannot See", Author = "Doerr, Anthony", PublicationYear = 2014,
+                CallNumber = "F DOERR"
+            },
+            new Book
+            {
+                Id = 5, Title = "The Girls", Author = "Cline, Ewna", PublicationYear = 2016, CallNumber = "F CLINE"
+            },
+
+            new Book
+            {
+                Id = 6, Title = "The Martian", Author = "weir, Andy", PublicationYear = 2011, CallNumber = "SF WEIR"
+            },
+
+            new Book
+            {
+                Id = 7, Title = "Me Before You", Author = "Moyes, Jojo", PublicationYear = 2012, CallNumber = "F MOYES"
+            },
+
+            new Book
+            {
+                Id = 8, Title = "Alexander Hamilton", Author = "Chernow, Ron", PublicationYear = 2004,
+                CallNumber = "B HAMILTO A"
+            },
+
+            new Book
+            {
+                Id = 9, Title = "Before the Fall", Author = "Hawley, Noah", PublicationYear = 2016,
+                CallNumber = "F HAWLE"
+            }
+        };
+
+        public IEnumerable<Book> Get()
+        {
+            return Books;
+        }
+
+        // GET api/values/5
+        //[Authorize]
+        public IHttpActionResult Get(int id)
+        {
+            var book = Books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
